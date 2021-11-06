e3e66315dd5de31849cf6370a22bf3cdc44d61bd
PrayagS <prayag.s@ahduni.edu.in>
2021-11-06 23:45:10 +0530

    feat: Use envar for showing the debug log
    Signed-off-by: PrayagS <prayag.s@ahduni.edu.in>

--- raft/util.go
+++ raft/util.go
@@ -1,12 +1,15 @@
 package raft
 
-import "log"
+import (
+	"log"
+	"os"
+)
 
 // Debugging
 const Debug = 1
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
-	if Debug > 0 {
+	if os.Getenv("DEBUG") == "1" {
 		log.Printf(format, a...)
 	}
 	return
