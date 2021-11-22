5360dd2ff61ca5ad4ace6a297f02a16ed655a2d6
PrayagS <prayag.s@ahduni.edu.in>
2021-11-22 11:03:27 +0530

    refactor: Use envars for debugging
    Signed-off-by: PrayagS <prayag.s@ahduni.edu.in>

--- raft/util.go
+++ raft/util.go
@@ -1,12 +1,15 @@
 package raft
 
-import "log"
+import (
+	"log"
+	"os"
+)
 
 // Debugging
 const Debug = 0
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
-	if Debug > 0 {
+	if os.Getenv("DEBUG") == "1" || Debug == 1 {
 		log.Printf(format, a...)
 	}
 	return
